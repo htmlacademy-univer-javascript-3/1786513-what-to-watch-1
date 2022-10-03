f618f5967b075cffbd7a15da989cd23e90ab4a82
Дейнов Кирилл Дмитриевич <deyinov@tochka.com>
2022-10-04 03:31:07 +0500

    Сделал главную страницу

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,7 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main, { MainProps } from '../../pages/main/main';
+
+function App({ name, genre, releaseYear }: MainProps): JSX.Element {
+  return <Main name={name} genre={genre} releaseYear={releaseYear} />;
 }
 
 export default App;
