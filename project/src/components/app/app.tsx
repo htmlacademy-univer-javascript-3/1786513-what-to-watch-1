223d9a2d51096e827e97f287eb323b2319dbcced
Дейнов Кирилл Дмитриевич <deyinov@tochka.com>
2022-10-11 04:54:50 +0500

    Добавил роутинг

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,7 +1,41 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main, { MainProps } from '../../pages/main/main';
+import NotFound from '../../pages/not-found/not-found';
+import PrivateRoute from '../private-route/private-route';
+import SignIn from '../../pages/sign-in/sign-in';
+import MyList from '../../pages/my-list/my-list';
+import Film from '../../pages/film/film';
+import AddReview from '../../pages/add-review/add-review';
+import Player from '../../pages/player/player';
 
 function App({ name, genre, releaseYear }: MainProps): JSX.Element {
-  return <Main name={name} genre={genre} releaseYear={releaseYear} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <Main name={name} genre={genre} releaseYear={releaseYear} />
+            }
+          />
+          <Route path="login" element={<SignIn />} />
+          <Route
+            path="mylist"
+            element={
+              <PrivateRoute>
+                <MyList />
+              </PrivateRoute>
+            }
+          />
+          <Route path="films/:id" element={<Film />} />
+          <Route path="films/:id/review" element={<AddReview />} />
+          <Route path="player/:id" element={<Player />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
